db947fba593841607217655a4270a0ec14d81da8
Chris M <habibdev96@gmail.com>
2021-01-06 22:40:44 -0700

    created dropdowns & changed bg img on faq section on homepage

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,10 @@
 "use strict";
+
+// FAQ DROPDOWN
+const faqLink = document.querySelectorAll(".faq-link");
+
+faqLink.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggle.nextElementSibling.classList.toggle("active");
+  });
+});
