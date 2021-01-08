90fd6a640f744ee4cd2ffd97ce64c93bae4ff653
Chris M <habibdev96@gmail.com>
2021-01-07 23:54:24 -0700

    created hamburger menu

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,5 +1,11 @@
 "use strict";
 
+// hamburger menu
+const menuIcon = document.querySelector(".hamburger-menu");
+const menuNavbar = document.querySelector(".menu-navbar");
+
+menuIcon.addEventListener("click", () => menuNavbar.classList.toggle("change"));
+
 // FAQ DROPDOWN
 const faqLink = document.querySelectorAll(".faq-link");
 
